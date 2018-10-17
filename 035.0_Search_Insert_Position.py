5fe5899de8691e0132fa31672de933612d3a82e9
Li Xiang <laoxing201314@outlook.com>
2018-10-17 11:36:17 +0800

    Update 035.0_Search_Insert_Position.py

---
+++ 035.0_Search_Insert_Position.py
@@ -0,0 +1,27 @@
+class Solution:
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        i = 0
+        for i, num in enumerate(nums):
+            if num >= target:
+                return i
+
+        return i + 1
+
+if __name__ == "__main__":
+    nums = [1,3,5,6]
+    target = 5
+    print(Solution().searchInsert(nums, target))
+
+    target = 2
+    print(Solution().searchInsert(nums, target))
+
+    target = 7
+    print(Solution().searchInsert(nums, target))
+
+    target = 0
+    print(Solution().searchInsert(nums, target))
