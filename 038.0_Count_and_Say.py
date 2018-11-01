c0467946618e1e2689d898a3f6a8439cfb17a914
Li Xiang <laoxing201314@outlook.com>
2018-11-01 12:46:27 +0800

    Update 038.0_Count_and_Say.py

---
+++ 038.0_Count_and_Say.py
@@ -0,0 +1,42 @@
+'''
+Author: lixiang
+Beats: 99.69%
+Solution: recursion
+'''
+
+class Solution:
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        if n == 1:
+            return "1"
+
+        s = self.countAndSay(n-1)
+        ret = ""
+        l = len(s)
+        if l > 0:
+            c = s[0]
+            count = 1
+            for i in range(1, l):
+                if c == s[i]:
+                    count += 1
+                else:
+                    ret += str(count) + c
+                    c = s[i]
+                    count = 1
+        ret += str(count) + s[-1::1]
+        return ret
+            
+
+
+if __name__ == "__main__":
+    n = 1
+    print(Solution().countAndSay(n))
+
+    n = 2
+    print(Solution().countAndSay(n))
+
+    n = 5
+    print(Solution().countAndSay(n))
