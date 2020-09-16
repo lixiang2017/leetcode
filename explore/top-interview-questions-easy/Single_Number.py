2f741bd52eb1ada413719b78aca9cd6c5457a0d0
lixiang <838255715@qq.com>
2020-09-16 09:18:28 +0800

    explore -- top interview questions easy

---
+++ explore/top-interview-questions-easy/Single_Number.py
@@ -0,0 +1,17 @@
+'''
+XOR : ^
+
+You are here!
+Your runtime beats 89.53 % of python submissions.
+'''
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        res = 0
+        for num in nums:
+            res = res ^ num
+        return res
