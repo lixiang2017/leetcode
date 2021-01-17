9ae49350ab8c4b97182cfd75db25abe0496b85f5
lixiang <838255715@qq.com>
2021-01-17 18:50:00 +0800

    explore 2021

---
+++ explore/january/Count_Sorted_Vowel_Strings.1.py
@@ -0,0 +1,44 @@
+'''
+approach: backtracking + memoization
+
+41 / 41 test cases passed.
+Status: Accepted
+Runtime: 28 ms
+Memory Usage: 13.5 MB
+Submitted: 0 minutes ago
+
+You are here!
+Your runtime beats 28.42 % of python submissions.
+You are here!
+Your memory usage beats 45.89 % of python submissions.
+'''
+
+
+class Solution(object):
+    def countVowelStrings(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return self.backtracking(["a","e","i","o","u"], [], n, 1)
+    
+    memo = {} 
+    # key: (current_vowel, left_vowels_to_add)
+    # value: count
+    
+    def backtracking(self, vowels, path, n, count):
+        if len(path) == n:
+            return count
+        
+        if path and (path[-1], n - len(path)) in self.memo:
+            return self.memo[(path[-1], n - len(path))]
+        
+        child_count = 0
+        for vowel in vowels:
+            if (path and ord(path[-1]) <= ord(vowel)) or (not path):
+                child_count += self.backtracking(vowels, path + [vowel], n, count)
+        
+        if path and (path[-1], n - len(path)) not in self.memo:
+            self.memo[(path[-1], n - len(path))] = child_count
+            
+        return child_count
