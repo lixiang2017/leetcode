fbcc54f258c54765b9a96e25ee87b15eb9055809
lixiang <838255715@qq.com>
2021-01-23 12:42:12 +0800

    explore 2021

---
+++ leetcode-cn/1319.0_Number_of_Operations_to_Make_Network_Connected.py
@@ -0,0 +1,51 @@
+'''
+approach: Union Find
+
+执行结果：
+通过
+显示详情
+执行用时：
+3016 ms, 在所有 Python 提交中击败了23.08%的用户
+内存消耗：
+27.8 MB, 在所有 Python 提交中击败了53.85%的用户
+'''
+
+
+class Solution(object):
+    def makeConnected(self, n, connections):
+        """
+        :type n: int
+        :type connections: List[List[int]]
+        :rtype: int
+        """
+        if len(connections) < n - 1: return -1
+
+        uf = UnionFind(n)
+        for conn in connections:
+            a, b = conn
+            uf.union(a, b)
+
+        return uf.getCount - 1
+
+
+class UnionFind(object):
+    def __init__(self, n):
+        self.parent = {i: i for i in range(n)}
+        # the number of connected components
+        self.getCount = n
+
+    def find(self, x):
+        if self.parent[x] == x:
+            return x
+        
+        return self.find(self.parent[x])
+        
+    
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX == rootY:
+            return
+        else:
+            self.parent[rootX] = rootY
+            self.getCount -= 1
