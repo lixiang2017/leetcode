2c23529d7d7b5f3f6658c31c45e8411b04cef201
lixiang <lixiang@rxthinking.com>
2021-02-15 10:26:16 +0800

    explore 2021

---
+++ leetcode-cn/0485.0_Max_Consecutive_Ones.py
@@ -0,0 +1,26 @@
+'''
+
+
+执行结果：
+通过
+显示详情
+执行用时：328 ms, 在所有 Python 提交中击败了32.34%的用户
+内存消耗：13.2 MB, 在所有 Python 提交中击败了88.89%的用户
+'''
+
+
+class Solution(object):
+    def findMaxConsecutiveOnes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        longest = current_length = 0
+        for num in nums:
+            if num == 1:
+                current_length += 1
+                longest = max(longest, current_length)
+            else:
+                current_length = 0
+
+        return longest
