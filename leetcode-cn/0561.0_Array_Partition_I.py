910dd900fcbff4e0a8d537f38cd08d4450a84d89
lixiang <lixiang@rxthinking.com>
2021-02-16 16:53:59 +0800

    explore 2021

--- leetcode-cn/0561.0_Array_Partition_I.py
+++ leetcode-cn/0561.0_Array_Partition_I.py
@@ -1,5 +1,5 @@
 '''
-approach: Sort
+approach: Sort + Greedy
 Time: O(NlogN)
 Space: O(1)
 
@@ -18,3 +18,20 @@ class Solution(object):
         """
         nums.sort()
         return sum(num for i, num in enumerate(nums) if not (i & 1))
+
+
+'''
+执行结果：
+通过
+显示详情
+执行用时：60 ms, 在所有 Python 提交中击败了95.41%的用户
+内存消耗：14.6 MB, 在所有 Python 提交中击败了27.55%的用户
+'''
+
+class Solution(object):
+    def arrayPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return sum(sorted(nums)[::2])
---
+++ leetcode-cn/1668.0_Maximum_Repeating_Substring.py
@@ -0,0 +1,53 @@
+'''
+
+执行结果：
+通过
+显示详情
+执行用时：16 ms, 在所有 Python 提交中击败了78.95%的用户
+内存消耗：13 MB, 在所有 Python 提交中击败了67.42%的用户
+'''
+
+
+class Solution(object):
+    def maxRepeating(self, sequence, word):
+        """
+        :type sequence: str
+        :type word: str
+        :rtype: int
+        """
+        repeat = 0
+        while word * (repeat + 1) in sequence:
+            repeat += 1
+        return repeat
+
+
+'''
+approach: Binary
+
+ref:
+https://leetcode-cn.com/problems/maximum-repeating-substring/solution/python-er-fen-fa-by-miaoch-mpee/
+
+执行结果：
+通过
+显示详情
+执行用时：12 ms, 在所有 Python 提交中击败了93.23%的用户
+内存消耗：12.9 MB, 在所有 Python 提交中击败了71.97%的用户
+'''
+
+
+class Solution(object):
+    def maxRepeating(self, sequence, word):
+        """
+        :type sequence: str
+        :type word: str
+        :rtype: int
+        """
+        L, R = 0, len(sequence) / len(word)
+        while L < R:
+            # mid = L + (R - L) // 2 # TLE
+            mid = L + (R - L + 1) // 2
+            if word * mid in sequence:
+                L = mid
+            else:
+                R = mid - 1
+        return L
