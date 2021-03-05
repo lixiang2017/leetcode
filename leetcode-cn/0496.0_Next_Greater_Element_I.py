da7c44c1ec6ba708153caf46f2e894858ff8d881
lixiang <838255715@qq.com>
2021-03-06 01:56:29 +0800

    explore 2021

--- leetcode-cn/0496.0_Next_Greater_Element_I.py
+++ leetcode-cn/0496.0_Next_Greater_Element_I.py
@@ -34,3 +34,33 @@ class Solution(object):
 
             i += 1
         return greater
+
+
+
+'''
+approach: Mono Stack
+Time: O(N + M + N) = O(M + N)
+Space: O(M)
+
+执行用时：20 ms, 在所有 Python 提交中击败了92.70%的用户
+内存消耗：13.1 MB, 在所有 Python 提交中击败了84.78%的用户
+'''
+class Solution(object):
+    def nextGreaterElement(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        greater = []
+        hashMap = {num: -1 for num in nums2}
+        stack = []
+        for num in nums2:
+            while stack and num > stack[-1]:
+                hashMap[stack.pop()] = num
+            stack.append(num)
+
+        for num in nums1:
+            greater.append(hashMap[num]) 
+
+        return greater
--- leetcode-cn/0503.0_Next_Greater_Element_II.py
+++ leetcode-cn/0503.0_Next_Greater_Element_II.py
@@ -33,3 +33,78 @@ class Solution(object):
             i += 1
 
         return greater
+
+
+
+'''
+wrong answer!
+There may be duplicate elements in nums, so we CANNOT keep num in stack.
+
+输入
+[100,1,11,1,120,111,123,1,-1,-100]
+输出
+[120,120,120,120,123,123,-1,120,100,100]
+预期结果
+[120,11,120,120,123,123,-1,100,100,100]
+stdout
+{1: 11, 100: -1, 11: -1, 111: -1, 120: -1, 123: -1, -100: -1, -1: -1}
+{1: 120, 100: -1, 11: -1, 111: -1, 120: -1, 123: -1, -100: -1, -1: -1}
+{1: 120, 100: -1, 11: 120, 111: -1, 120: -1, 123: -1, -100: -1, -1: -1}
+{1: 120, 100: 120, 11: 120, 111: -1, 120: -1, 123: -1, -100: -1, -1: -1}
+{1: 120, 100: 120, 11: 120, 111: 123, 120: -1, 123: -1, -100: -1, -1: -1}
+{1: 120, 100: 120, 11: 120, 111: 123, 120: 123, 123: -1, -100: -1, -1: -1}
+{1: 120, 100: 120, 11: 120, 111: 123, 120: 123, 123: -1, -100: 100, -1: -1}
+{1: 120, 100: 120, 11: 120, 111: 123, 120: 123, 123: -1, -100: 100, -1: 100}
+{1: 100, 100: 120, 11: 120, 111: 123, 120: 123, 123: -1, -100: 100, -1: 100}
+{1: 11, 100: 120, 11: 120, 111: 123, 120: 123, 123: -1, -100: 100, -1: 100}
+{1: 120, 100: 120, 11: 120, 111: 123, 120: 123, 123: -1, -100: 100, -1: 100}
+{1: 120, 100: 120, 11: 120, 111: 123, 120: 123, 123: -1, -100: 100, -1: 100}
+{1: 120, 100: 120, 11: 120, 111: 123, 120: 123, 123: -1, -100: 100, -1: 100}
+{1: 120, 100: 120, 11: 120, 111: 123, 120: 123, 123: -1, -100: 100, -1: 100}
+{1: 120, 100: 120, 11: 120, 111: 123, 120: 123, 123: -1, -100: 100, -1: 100}
+'''
+class Solution(object):
+    def nextGreaterElements(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        N = len(nums)
+        hashMap = {num: -1 for num in nums}
+        stack = []
+        for i in range(2 * N):
+            while stack and nums[i % N] > stack[-1]:
+                hashMap[stack.pop()] = nums[i % N]
+                print hashMap
+            stack.append(nums[i % N])
+
+        return [hashMap[num] for num in nums]
+
+
+
+
+
+'''
+approach: Mono Stack, keep index in stack
+Time: O(2 * N) = O(N)
+Space: O(N)
+
+执行用时：196 ms, 在所有 Python 提交中击败了71.50%的用户
+内存消耗：15.2 MB, 在所有 Python 提交中击败了21.00%的用户
+'''
+
+class Solution(object):
+    def nextGreaterElements(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        N = len(nums)
+        hashMap = [-1] * N
+        stack = []
+        for i in range(2 * N):
+            while stack and nums[i % N] > nums[stack[-1]]:
+                hashMap[stack.pop()] = nums[i % N]
+            stack.append(i % N)
+
+        return hashMap
