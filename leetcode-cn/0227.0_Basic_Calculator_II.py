ce4a1810b19c7d21217bb84e4631d3b8ef1b6000
lixiang <lixiang@rxthinking.com>
2021-03-10 23:09:17 +0800

    explore 2021

---
+++ leetcode-cn/0227.0_Basic_Calculator_II.py
@@ -0,0 +1,14 @@
+'''
+
+执行用时：88 ms, 在所有 Python 提交中击败了88.24%的用户
+内存消耗：54.7 MB, 在所有 Python 提交中击败了13.97%的用户
+'''
+
+
+class Solution(object):
+    def calculate(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        return eval(s)
