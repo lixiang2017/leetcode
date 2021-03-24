c97f035dec1987ba397c23ffcb19b40e3e56e981
lixiang <lixiang@rxthinking.com>
2021-03-24 18:55:23 +0800

    explore 2021

--- leetcode-cn/0456.0_132_Pattern.py
+++ leetcode-cn/0456.0_132_Pattern.py
@@ -27,3 +27,40 @@ class Solution(object):
                     return True
 
         return False
+
+
+'''
+approach: Greedy
+Time: O(N^2)
+Space: O(1)
+
+ref:
+https://www.cnblogs.com/grandyang/p/6081984.html
+
+执行用时：3032 ms, 在所有 Python 提交中击败了12.66%的用户
+内存消耗：13.6 MB, 在所有 Python 提交中击败了79.75%的用户
+'''
+
+class Solution(object):
+    def find132pattern(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        N = len(nums)
+        i = j = k = 0
+        while i < N:
+            while i + 1 < N and nums[i] >= nums[i + 1]:
+                i += 1
+            j = i + 1
+            while j + 1 < N and nums[j] <= nums[j + 1]:
+                j += 1
+            k = j + 1
+            while k < N:
+                if nums[i] < nums[k] and nums[k] < nums[j]:
+                    return True
+                k += 1
+
+            i = j + 1
+
+        return False
