7923014991082a0e4c44dc9c487fb0c11ef8a244
lixiang <838255715@qq.com>
2021-04-09 20:41:54 +0800

    explore 2021

---
+++ leetcode-cn/0154.0_Find_Minimum_in_Rotated_Sorted_Array_II.py
@@ -0,0 +1,16 @@
+'''
+Time: O(N)
+Space :O(1)
+
+执行用时：12 ms, 在所有 Python 提交中击败了97.20%的用户
+内存消耗：13.2 MB, 在所有 Python 提交中击败了88.80%的用户
+'''
+
+
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return min(nums)
