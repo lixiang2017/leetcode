b1680670191b1c037fc13062ae3c72147b6638cb
lixiang <838255715@qq.com>
2021-04-14 00:45:52 +0800

    explore 2021

---
+++ explore/april/Flatten_Nested_List_Iterator.2.py
@@ -0,0 +1,55 @@
+'''
+
+You are here!
+Your runtime beats 57.27 % of python3 submissions.
+
+'''
+
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def isInteger(self) -> bool:
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        """
+#
+#    def getInteger(self) -> int:
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        """
+#
+#    def getList(self) -> [NestedInteger]:
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        """
+
+class NestedIterator:
+    def __init__(self, nestedList: [NestedInteger]):
+        self.it = self.generator(nestedList)
+        self.number = None
+    
+    def generator(self, nodes):
+        for node in nodes:
+            if node.isInteger():
+                yield node.getInteger()
+            else:
+                yield from self.generator(node.getList())
+    
+    def next(self) -> int:
+        return self.number
+    
+    def hasNext(self) -> bool:
+        try:
+            # self.number = next(self.it, None)  # will be wrong when using try-except
+            self.number = next(self.it)
+            return True
+        except:
+            return False
+
+# Your NestedIterator object will be instantiated and called as such:
+# i, v = NestedIterator(nestedList), []
+# while i.hasNext(): v.append(i.next())
