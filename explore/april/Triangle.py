c1e8be4c45b8ab4ecc8b589df641a1a25169f9cd
lixiang <lixiang@rxthinking.com>
2021-04-21 17:24:44 +0800

    explore 2021

--- explore/april/Triangle.py
+++ explore/april/Triangle.py
@@ -26,4 +26,25 @@ class Solution:
                     sums[j] = min(presums[j] + triangle[i][j], presums[j - 1] + triangle[i][j]) 
             
         return min(sums)
-        
+
+
+
+
+'''
+approach: Thinking Reversely, add from bottom to top
+Time: O(N^2)
+Space: O(1)
+
+You are here!
+Your runtime beats 59.65 % of python3 submissions.
+You are here!
+Your memory usage beats 66.62 % of python3 submissions.
+'''
+
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        N = len(triangle)
+        for i in range(N - 2, -1, -1):
+            for j in range(i + 1):
+                triangle[i][j] += min(triangle[i + 1][j], triangle[i + 1][j + 1])
+        return triangle[0][0]
