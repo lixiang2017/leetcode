5f9564dea3bb0f58ca800a0c8e1fe379c387aea5
lixiang <838255715@qq.com>
2021-04-22 00:10:19 +0800

    explore 2021

--- leetcode-cn/0144.0_Binary_Tree_Preorder_Traversal.py
+++ leetcode-cn/0144.0_Binary_Tree_Preorder_Traversal.py
@@ -28,3 +28,24 @@ class Solution:
                 stack.append(node.left)
 
         return output
+
+
+
+'''
+approach: One Line / DFS
+Time: O(N)
+Space: O(N)
+
+执行用时：56 ms, 在所有 Python3 提交中击败了5.44%的用户
+内存消耗：14.8 MB, 在所有 Python3 提交中击败了57.25%的用户
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        return list(chain([root.val], self.preorderTraversal(root.left), self.preorderTraversal(root.right))) if root else []
