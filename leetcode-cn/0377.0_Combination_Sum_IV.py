2cc1d25d275b85a4cdaeb6eb68af00c4603bb7ae
lixiang <838255715@qq.com>
2021-04-24 13:47:13 +0800

    explore 2021

---
+++ leetcode-cn/0377.0_Combination_Sum_IV.py
@@ -0,0 +1,28 @@
+'''
+approach: DFS + Cache / Recursion + Memoization
+Time: O(target)
+Space: O(target)
+
+执行用时：52 ms, 在所有 Python3 提交中击败了54.81%的用户
+内存消耗：15.1 MB, 在所有 Python3 提交中击败了8.57%的用户
+'''
+
+
+from functools import cache
+
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+
+        @cache
+        def dfs(target):
+            if target < 0:
+                return 0
+            if 0 == target:
+                return 1
+
+            ans = 0
+            for num in nums:
+                ans += dfs(target - num)
+            return ans
+
+        return dfs(target)
