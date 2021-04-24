02b94cd58e901aa4e11af8782eebec82892fcdff
lixiang <838255715@qq.com>
2021-04-24 13:51:14 +0800

    explore 2021

--- leetcode-cn/0377.0_Combination_Sum_IV.py
+++ leetcode-cn/0377.0_Combination_Sum_IV.py
@@ -26,3 +26,27 @@ class Solution:
             return ans
 
         return dfs(target)
+
+'''
+执行用时：44 ms, 在所有 Python3 提交中击败了90.69%的用户
+内存消耗：15.6 MB, 在所有 Python3 提交中击败了6.77%的用户
+'''
+from functools import cache, lru_cache
+
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+
+        # @cache
+        @lru_cache(maxsize=1005)
+        def dfs(target):
+            if target < 0:
+                return 0
+            if 0 == target:
+                return 1
+
+            ans = 0
+            for num in nums:
+                ans += dfs(target - num)
+            return ans
+
+        return dfs(target)
