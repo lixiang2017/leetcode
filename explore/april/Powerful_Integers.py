cfe2d03e74e6e47d949348041b2c64199256c4e3
lixiang <lixiang@rxthinking.com>
2021-05-01 14:34:51 +0800

    explore 2021

--- explore/april/Powerful_Integers.py
+++ explore/april/Powerful_Integers.py
@@ -37,3 +37,46 @@ class Solution:
 10
 '''
 
+
+'''
+approach: Set
+
+Runtime: 32 ms, faster than 68.44% of Python3 online submissions for Powerful Integers.
+Memory Usage: 14.4 MB, less than 30.33% of Python3 online submissions for Powerful Integers.
+'''
+
+class Solution:
+    def powerfulIntegers(self, x: int, y: int, bound: int) -> List[int]:
+        powerx = []
+        powery = []
+        px = 1
+        while px <= bound:
+            powerx.append(px)
+            px *= x
+            if 1 == x:
+                break
+                
+        py = 1
+        while py <= bound:
+            powery.append(py)
+            py *= y
+            if 1 == y:
+                break
+        
+        powers = set()
+        for px in powerx:
+            for py in powery:
+                if px + py <= bound:
+                    powers.add(px + py)
+                else:
+                    break
+        return list(powers)
+ 
+'''
+2
+1
+10
+'''
+
+
+
--- leetcode-cn/0690.0_Employee_Importance.py
+++ leetcode-cn/0690.0_Employee_Importance.py
@@ -1,5 +1,5 @@
 '''
-approach: Queue
+approach: Hash Table + Queue/BFS
 Time: O(N)
 Space: O(N)
 
@@ -28,3 +28,37 @@ class Solution:
             queue.extend(id2employee[employee_id].subordinates)
 
         return total
+
+
+
+'''
+approach: Hash Table + DFS
+Time: O(N)
+Space: O(N)
+
+执行用时：184 ms, 在所有 Python3 提交中击败了35.90%的用户
+内存消耗：16.2 MB, 在所有 Python3 提交中击败了15.56%的用户
+'''
+
+"""
+# Definition for Employee.
+class Employee:
+    def __init__(self, id: int, importance: int, subordinates: List[int]):
+        self.id = id
+        self.importance = importance
+        self.subordinates = subordinates
+"""
+
+class Solution:
+    def getImportance(self, employees: List['Employee'], id: int) -> int:
+        id2employee = {employee.id: employee for employee in employees}
+        def dfs(id):
+            total = 0
+            total += id2employee[id].importance
+            subs = id2employee[id].subordinates
+            if subs:
+                for sub in subs:
+                    total += dfs(sub)
+            return total
+
+        return dfs(id)
