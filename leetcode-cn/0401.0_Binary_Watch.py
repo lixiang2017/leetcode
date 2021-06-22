9e9400c5e211e80bd5dae78be1d5ae3eb23a95a4
lixiang <838255715@qq.com>
2021-06-22 09:17:53 +0800

    explore 2021

--- leetcode-cn/0401.0_Binary_Watch.py
+++ leetcode-cn/0401.0_Binary_Watch.py
@@ -1,4 +1,5 @@
 '''
+approach: combinations
 
 执行用时：44 ms, 在所有 Python3 提交中击败了58.99%的用户
 内存消耗：15.2 MB, 在所有 Python3 提交中击败了5.22%的用户
@@ -53,3 +54,40 @@ class Solution:
                       
         return combines
 
+
+
+'''
+approach: Enumerate hour and minute
+T: O(1)
+S: O(1)
+
+执行用时：44 ms, 在所有 Python3 提交中击败了58.99% 的用户
+内存消耗：14.9 MB, 在所有 Python3 提交中击败了50.69% 的用户
+'''
+
+class Solution:
+    def readBinaryWatch(self, turnedOn: int) -> List[str]:
+        ans = []
+        for h in range(12):
+            for m in range(60):
+                if bin(h).count('1') + bin(m).count('1') == turnedOn:
+                    ans.append(f'{h}:{m:02d}')
+        
+        return ans
+
+
+'''
+Enumerate 2^10,T:O(1),S:O(1)
+
+执行用时：40 ms, 在所有 Python3 提交中击败了78.49% 的用户
+内存消耗：15 MB, 在所有 Python3 提交中击败了20.74% 的用户
+'''
+class Solution:
+    def readBinaryWatch(self, turnedOn: int) -> List[str]:
+        ans = []
+        for i in range(1024):
+            h, m = i >> 6, i & 63
+            if h < 12 and m < 60 and bin(i).count('1') == turnedOn:
+                ans.append(f'{h}:{m:02d}')
+        
+        return ans
