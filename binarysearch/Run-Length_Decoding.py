e6dc3df9fd8913893f3e91a2f2c1bb7ad76d0783
lixiang <lixiang@rxthinking.com>
2021-07-06 19:04:13 +0800

    explore 2021

--- binarysearch/Run-Length_Decoding.py
+++ binarysearch/Run-Length_Decoding.py
@@ -16,4 +16,20 @@ class Solution:
 
         return ans
         # return ''.join(int(s[i]) * s[i + 1] for i in range(0, len(s), 2))
-        
+
+
+'''
+Your code took 2 milliseconds — faster than 100.00% in Python
+'''
+class Solution:
+    def solve(self, s):
+        output = ''
+        num = ''
+        for ch in s:
+            if ch.isalpha():
+                output += ch * int(num)
+                num = ''
+            else:
+                num += ch
+
+        return output        
