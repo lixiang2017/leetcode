8db5a437f7bc32f715db9e2baa4c56c5f4bed7dd
lixiang <lixiang@rxthinking.com>
2021-07-06 19:01:16 +0800

    explore 2021

---
+++ binarysearch/Run-Length_Decoding.py
@@ -0,0 +1,19 @@
+'''
+Your code took 2 milliseconds — faster than 100.00% in Python
+'''
+
+class Solution:
+    def solve(self, s):
+        ans = ''
+        cnt = 0
+        for ch in s:
+            if 'a' <= ch <= 'z':
+                ans += ch * cnt
+                cnt = 0
+            else:
+                cnt *= 10
+                cnt += int(ch)
+
+        return ans
+        # return ''.join(int(s[i]) * s[i + 1] for i in range(0, len(s), 2))
+        
