9225c84c004b864aa41d5f63c35d357e3c5f8b5a
lixiang <lixiang@rxthinking.com>
2021-07-06 16:47:11 +0800

    explore 2021

--- explore/july/Reduce_Array_Size_to_The_Half.py
+++ explore/july/Reduce_Array_Size_to_The_Half.py
@@ -8,7 +8,6 @@ Your runtime beats 46.93 % of python3 submissions.
 You are here!
 Your memory usage beats 19.55 % of python3 submissions.
 '''
-
 class Solution:
     def minSetSize(self, arr: List[int]) -> int:
         N = len(arr)
@@ -21,3 +20,71 @@ class Solution:
             if total >= N // 2:
                 break
         return least
+
+
+'''
+You are here!
+Your runtime beats 44.83 % of python3 submissions.
+'''
+from collections import Counter, OrderedDict
+class Solution:
+    def minSetSize(self, arr: List[int]) -> int:
+        N = len(arr)
+        freq = Counter(arr)
+        ordered = OrderedDict(sorted(freq.items(), key=lambda x: -x[1]))
+        total = least = 0
+        for _, cnt in ordered.items():
+            total += cnt
+            least += 1
+            if total >= N // 2:
+                break
+        return least
+
+
+'''
+You are here!
+Your runtime beats 91.06 % of python3 submissions.
+You are here!
+Your memory usage beats 77.79 % of python3 submissions.
+'''
+from collections import Counter, OrderedDict
+
+class Solution:
+    def minSetSize(self, arr: List[int]) -> int:
+        N = len(arr)
+        freq = Counter(arr)
+        times = sorted(freq.values(), key=lambda x: -x)
+        total = least = 0
+        for t in times:
+            total += t
+            least += 1
+            if total >= N // 2:
+                break
+        return least
+
+
+'''
+You are here!
+Your runtime beats 99.72 % of python3 submissions.
+'''
+from collections import Counter, OrderedDict
+
+class Solution:
+    def minSetSize(self, arr: List[int]) -> int:
+        N = len(arr)
+        freq = Counter(arr)
+        # times = sorted(freq.values(), key=lambda x: -x)
+        times = list(freq.values())
+        times.sort(reverse=True)
+        total = least = 0
+        for t in times:
+            total += t
+            least += 1
+            if total >= N // 2:
+                break
+        return least
+        
+
+
+
+     
