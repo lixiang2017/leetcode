be605afa42c8c7139f17ff7ed8d4dcc1b45ca7dc
lixiang <838255715@qq.com>
2021-07-13 23:56:18 +0800

    explore 2021

---
+++ leetcode-cn/1929.0_Concatenation_of_Array.py
@@ -0,0 +1,16 @@
+'''
+执行用时：36 ms, 在所有 Python3 提交中击败了38.78% 的用户
+内存消耗：15.1 MB, 在所有 Python3 提交中击败了51.26% 的用户
+'''
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return nums + nums
+
+
+'''
+执行用时：36 ms, 在所有 Python3 提交中击败了38.78% 的用户
+内存消耗：15.1 MB, 在所有 Python3 提交中击败了47.15% 的用户
+'''
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return nums * 2
