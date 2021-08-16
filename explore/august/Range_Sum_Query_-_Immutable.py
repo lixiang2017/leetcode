4cdb03cf16d596eead3805f3bf0ed00cb1ca07d6
lixiang2017 <laoxing201314@outlook.com>
2021-08-16 19:25:37 +0800

    explore 2021

--- explore/august/Range_Sum_Query_-_Immutable.py
+++ explore/august/Range_Sum_Query_-_Immutable.py
@@ -42,3 +42,25 @@ class NumArray:
 # Your NumArray object will be instantiated and called as such:
 # obj = NumArray(nums)
 # param_1 = obj.sumRange(left,right)
+
+
+'''
+You are here!
+Your runtime beats 81.05 % of python3 submissions.
+You are here!
+Your memory usage beats 21.76 % of python3 submissions.
+'''
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.sum = [0] + list(accumulate(nums))
+
+    def sumRange(self, left: int, right: int) -> int:
+            return self.sum[right + 1] - self.sum[left]
+
+        
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
+
+
