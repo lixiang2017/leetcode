8039680fd03439f938b998b9eeac6899420bc212
lixiang2017 <laoxing201314@outlook.com>
2021-08-31 19:44:19 +0800

    explore 2021

---
+++ explore/august/Find_Minimum_in_Rotated_Sorted_Array.py
@@ -0,0 +1,10 @@
+'''
+Time: O(N)
+Space: O(1)
+
+You are here!
+Your runtime beats 35.37 % of python3 submissions.
+'''
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        return min(nums)
