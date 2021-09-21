78920a12883128281679d2f51e0b6aeac98db868
lixiang <838255715@qq.com>
2021-09-22 01:11:28 +0800

    explore 2021

--- leetcode-cn/CrackingTheCodingInterview/16.20.0_T9_LCCI.py
+++ leetcode-cn/CrackingTheCodingInterview/16.20.0_T9_LCCI.py
@@ -26,5 +26,25 @@ class Solution:
                     break
             if valid:
                 ans.append(w)
-
         return ans
+
+
+
+'''
+执行用时：76 ms, 在所有 Python3 提交中击败了26.22% 的用户
+内存消耗：16.4 MB, 在所有 Python3 提交中击败了5.49% 的用户
+通过测试用例：33 / 33
+'''
+class Solution:
+    def getValidT9Words(self, num: str, words: List[str]) -> List[str]:
+        # key = [2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 
+        # 7, 7, 7, 7, 8, 8, 8, 9, 9, 9, 9]
+        key = ['2', '2', '2', '3', '3', '3', '4', '4', '4', '5', '5', '5',
+         '6', '6', '6', '7', '7', '7', '7', '8', '8', '8', '9', '9', '9', '9']
+        ans = []
+        for w in words:
+            dec = ''.join([key[ord(ch) - ord('a')] for ch in w])
+            if dec == num:
+                ans.append(w)
+        return ans
+        
