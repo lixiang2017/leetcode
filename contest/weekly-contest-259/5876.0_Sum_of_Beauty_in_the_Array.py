a7e9fe2dc3381ebee393c1b7b017450a8770f15a
lixiang2017 <laoxing201314@outlook.com>
2021-09-22 18:47:38 +0800

    explore 2021

--- contest/weekly-contest-259/5876.0_Sum_of_Beauty_in_the_Array.py
+++ contest/weekly-contest-259/5876.0_Sum_of_Beauty_in_the_Array.py
@@ -1,5 +1,15 @@
 '''
-AC
+前缀最大值+后缀最小值
+premax + postmin
+T: O(2N)
+S: O(N)
+
+比赛时误以为AC了，其实是写错了，没通过。
+
+执行用时：316 ms, 在所有 Python3 提交中击败了70.26%的用户
+内存消耗：25 MB, 在所有 Python3 提交中击败了79.64%的用户
+通过测试用例：
+62 / 62
 '''
 class Solution:
     def sumOfBeauties(self, nums: List[int]) -> int:
@@ -8,7 +18,7 @@ class Solution:
         premax = nums[0]
         postmin = [nums[-1]] * N
         for i in range(N - 2, -1, -1):
-            postmin[i] = min(postmin[i+1], nums[i + 1])
+            postmin[i] = min(postmin[i+1], nums[i])
         
         for i in range(1, N - 1):
             premax = max(premax, nums[i - 1])
