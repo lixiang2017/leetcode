c44e8c82f5db979e930d521e3c3aa985c26211d9
lixiang2017 <laoxing201314@outlook.com>
2021-09-28 20:14:51 +0800

    explore 2021

--- cpp/Sort_Array_By_Parity_II.cpp
+++ cpp/Sort_Array_By_Parity_II.cpp
@@ -1,9 +1,15 @@
 /*
+Two Pointers
+T: O(N)
+S: O(1)
 
 You are here!
 Your runtime beats 98.31 % of cpp submissions.
 You are here!
 Your memory usage beats 98.57 % of cpp submissions
+
+i: even
+j: odd
 */
 class Solution {
 public:
@@ -11,7 +17,7 @@ public:
         int n = nums.size();
         int j = 1;
         for (int i = 0; i < n; i += 2) {
-            if (nums[i] & 1) {
+            if (nums[i] & 1) {                 // nums[i] % 2
                 while (nums[j] & 1) {
                     j += 2;
                 }
--- explore/september/Sort_Array_By_Parity_II.py
+++ explore/september/Sort_Array_By_Parity_II.py
@@ -1,4 +1,8 @@
 '''
+Two Pointers
+T: O(N^2)
+S: O(1)
+
 61 / 61 test cases passed.
     Status: Accepted
 Runtime: 516 ms
@@ -29,7 +33,7 @@ class Solution:
 Two Pointers
 T: O(N)
 S: O(1)
-ss
+
 You are here!
 Your runtime beats 54.85 % of python3 submissions.
 You are here!
@@ -46,3 +50,38 @@ class Solution:
                 nums[i], nums[j] = nums[j], nums[i]
             i += 2
         return nums
+
+
+'''
+Two Pointers
+T: O(N)
+S: O(1)
+
+You are here!
+Your runtime beats 30.34 % of python3 submissions.
+You are here!
+Your memory usage beats 88.11 % of python3 submissions
+'''
+class Solution:
+    def sortArrayByParityII(self, nums: List[int]) -> List[int]:
+        l = r = 0
+        while l < len(nums):
+            while l < len(nums) and l & 1 == nums[l] & 1:
+                l += 1
+            if l >= len(nums):
+                break
+            if r == 0 or r == l:
+                r = l + 1
+
+            while r < len(nums) and ((l & 1 != nums[r] & 1) or (r & 1 == l & 1)):
+                r += 1
+            if r >= len(nums):
+                break
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+
+        return nums
+
+
+
+
