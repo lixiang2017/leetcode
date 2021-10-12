8b66ffb64bc6e013289a2d9406b8225f14ca7038
lixiang2017 <laoxing201314@outlook.com>
2021-10-12 13:53:47 +0800

    explore 2021

---
+++ cpp/0543.0_Diameter_of_Binary_Tree.cpp
@@ -0,0 +1,36 @@
+/*
+DFS
+
+Runtime: 16 ms, faster than 33.05% of C++ online submissions for Diameter of Binary Tree.
+Memory Usage: 20.3 MB, less than 26.51% of C++ online submissions for Diameter of Binary Tree.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        int diameter = 0;
+        height(root, diameter);
+        return diameter;
+    }
+private:
+    int height(TreeNode* node, int& diameter) {
+        if (!node) {
+            return 0;
+        }
+        int lh = height(node->left, diameter);
+        int rh = height(node->right, diameter);
+        diameter = max(diameter, lh + rh);
+        return 1 + max(lh, rh);
+    }
+};
---
+++ java/0543.0_Diameter_of_Binary_Tree.java
@@ -0,0 +1,39 @@
+/*
+DFS
+
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Diameter of Binary Tree.
+Memory Usage: 39.1 MB, less than 45.85% of Java online submissions for Diameter of Binary Tree.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int diameterOfBinaryTree(TreeNode root) {
+        int [] diameter = new int[1];
+        height(root, diameter);
+        return diameter[0];
+    }
+    
+    private int height(TreeNode node, int[] diameter) {
+        if (node == null) {
+            return 0;
+        }
+        int lh = height(node.left, diameter);
+        int rh = height(node.right, diameter);
+        diameter[0] = Math.max(diameter[0], lh + rh);
+        return 1 + Math.max(lh, rh);
+    }
+}
