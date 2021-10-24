e284e22c313ade8bc7985b01dec7878d52992979
lixiang <838255715@qq.com>
2021-10-24 22:21:37 +0800

    explore 2021

--- leetcode-cn/0638.0_Shopping_Offers.py
+++ leetcode-cn/0638.0_Shopping_Offers.py
@@ -60,3 +60,33 @@ class Solution:
         return optimal
 
 
+'''
+sum
+backtracking
+
+执行用时：56 ms, 在所有 Python3 提交中击败了76.47% 的用户
+内存消耗：15.1 MB, 在所有 Python3 提交中击败了82.35% 的用户
+通过测试用例：64 / 64
+'''
+class Solution:
+    def shoppingOffers(self, price: List[int], special: List[List[int]], needs: List[int]) -> int:
+        optimal = sum(p * n for p, n in zip(price, needs))
+        special = [s for s in special if all(ss <= n for ss, n in zip(s, needs))]
+
+        def backtracking(cost, to_buy):
+            nonlocal optimal
+            if cost >= optimal:
+                return 
+            if set(to_buy) == {0}:
+                optimal = min(optimal, cost)
+                return
+            # special buy
+            for s in special:
+                if all(ss <= t for ss, t in zip(s, to_buy)):
+                    backtracking(cost + s[-1], [t - ss for ss, t in zip(s, to_buy)])
+            # normal buy
+            backtracking(cost + sum(p * t for p, t in zip(price, to_buy)), [0])
+
+        backtracking(0, needs)
+        return optimal
+
