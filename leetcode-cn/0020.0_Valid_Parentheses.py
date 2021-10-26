32b84e1e5b18d743dd406249ada104a9cb0b7796
lixiang <838255715@qq.com>
2021-10-27 01:05:12 +0800

    explore 2021

--- leetcode-cn/0020.0_Valid_Parentheses.py
+++ leetcode-cn/0020.0_Valid_Parentheses.py
@@ -59,4 +59,40 @@ class Solution:
                 stack.append(ch)
 
         return not stack
+
+
+'''
+stack, T: O(N), S: O(N)
+
+执行用时：32 ms, 在所有 Python3 提交中击败了70.28% 的用户
+内存消耗：15.1 MB, 在所有 Python3 提交中击败了5.65% 的用户
+通过测试用例：91 / 91
+'''
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        pair = {')': '(', ']': '[', '}': '{'}
+        left = {'(', '[', '{'}
+        for ch in s:
+            if ch in left:
+                stack.append(ch)
+            else:
+                if stack and pair[ch] == stack[-1]:
+                    stack.pop()
+                else:
+                    return False
+        return not stack
+
+
+
+'''
+执行用时：48 ms, 在所有 Python3 提交中击败了8.22% 的用户
+内存消耗：15.1 MB, 在所有 Python3 提交中击败了13.25% 的用户
+通过测试用例：91 / 91
+'''
+class Solution:
+    def isValid(self, s: str) -> bool:
+        while '()' in s or '[]' in s or '{}' in s:
+            s = s.replace('()', '').replace('[]', '').replace('{}', '')
+        return s == ''
         
