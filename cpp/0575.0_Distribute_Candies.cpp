c3342a8e37fa6bce84b84bfeaf0c123cc367e8ed
lixiang <838255715@qq.com>
2021-11-01 01:10:27 +0800

    explore 2021

--- cpp/0575.0_Distribute_Candies.cpp
+++ cpp/0575.0_Distribute_Candies.cpp
@@ -16,3 +16,32 @@ public:
         return min(candyType.size() / 2, s.size());
     }
 };
+
+
+
+/*
+执行用时：208 ms, 在所有 C++ 提交中击败了71.31% 的用户
+内存消耗：107.7 MB, 在所有 C++ 提交中击败了64.83% 的用户
+通过测试用例：206 / 206
+*/
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        unordered_set<int> s(candyType.begin(), candyType.end());
+        return min(candyType.size() / 2, s.size());
+    }
+};
+
+
+/*
+执行用时：288 ms, 在所有 C++ 提交中击败了27.87% 的用户
+内存消耗：117.3 MB, 在所有 C++ 提交中击败了15.98% 的用户
+通过测试用例：206 / 206
+*/
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        set<int> s(candyType.begin(), candyType.end());
+        return min(candyType.size() / 2, s.size());
+    }
+};
