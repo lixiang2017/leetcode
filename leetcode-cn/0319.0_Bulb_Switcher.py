d1bee96a7f9ffbb6e9917569744d5b04bb74c081
lixiang <838255715@qq.com>
2021-11-15 00:55:16 +0800

    explore 2021

---
+++ leetcode-cn/0319.0_Bulb_Switcher.py
@@ -0,0 +1,9 @@
+'''
+执行用时：32 ms, 在所有 Python3 提交中击败了63.32% 的用户
+内存消耗：15 MB, 在所有 Python3 提交中击败了31.52% 的用户
+通过测试用例：35 / 35
+'''
+class Solution:
+    def bulbSwitch(self, n: int) -> int:
+        return int(n ** 0.5)
+        
