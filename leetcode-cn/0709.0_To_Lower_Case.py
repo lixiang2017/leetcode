fac1b74d5a0e086f3cdff2ff925971b9766ba56d
lixiang2017 <laoxing201314@outlook.com>
2021-12-12 00:48:26 +0800

    explore 2021

--- leetcode-cn/0709.0_To_Lower_Case.py
+++ leetcode-cn/0709.0_To_Lower_Case.py
@@ -47,3 +47,49 @@ class Solution(object):
         """
         return ''.join([chr(ord(letter) - ord('A') + ord('a')) \
             if (ord('A') <= ord(letter) <= ord('Z')) else letter for letter in str])
+
+'''
+执行用时：28 ms, 在所有 Python3 提交中击败了85.80% 的用户
+内存消耗：15 MB, 在所有 Python3 提交中击败了33.78% 的用户
+通过测试用例：114 / 114
+'''
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        return s.lower()
+
+'''
+执行用时：24 ms, 在所有 Python3 提交中击败了95.97% 的用户
+内存消耗：14.9 MB, 在所有 Python3 提交中击败了81.19% 的用户
+通过测试用例：114 / 114
+'''
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        def to_low(ch):
+            if ord('A') <= ord(ch) <= ord('Z'):
+                return chr(ord(ch) - ord('A') + ord('a'))
+            return ch 
+        return ''.join(map(to_low, s))
+
+
+
+'''
+执行用时：32 ms, 在所有 Python3 提交中击败了63.15% 的用户
+内存消耗：14.9 MB, 在所有 Python3 提交中击败了48.94% 的用户
+通过测试用例：114 / 114
+'''
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        to_low = lambda ch: [ch, chr(ord(ch) - ord('A') + ord('a'))][ord('A') <= ord(ch) <= ord('Z')]
+        return ''.join(map(to_low, s))
+
+
+'''
+执行用时：32 ms, 在所有 Python3 提交中击败了63.15% 的用户
+内存消耗：14.9 MB, 在所有 Python3 提交中击败了70.06% 的用户
+通过测试用例：114 / 114
+'''
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        return ''.join(chr(ord(ch) | 32) if ord("A") <= ord(ch) <= ord('Z') else ch for ch in s)
+
+              
