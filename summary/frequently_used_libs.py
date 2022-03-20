d8be84d817c6b7b4f8c6e77bacc1bcbadbbbaff4
lixiang2017 <laoxing201314@outlook.com>
2022-03-20 21:12:51 +0800

    explore 2022

---
+++ summary/frequently_used_libs.py
@@ -0,0 +1,36 @@
+# from Queue import PriorityQueue   # Python 2
+
+from bisect import bisect_left, bisect_right
+from collections import Counter, OrderedDict, deque, defaultdict
+from copy import copy, deepcopy
+
+# from functools import Fraction, gcd, cache, lru_cache, reduce, cmp_to_key
+
+from heapq import heapify, heappop, heappush, merge, nsmallest
+from itertools import accumulate, chain, combinations, groupby, permutations, product, tee
+
+# from itertools import izip_longest
+
+from math import ceil, log, gcd, log, log2, sin, cos, pi, sqrt
+from operator import mul, ne, or_, xor
+from pprint import pprint
+from random import random
+from sortedcontainers import SortedDict, SortedList
+from string import ascii_letters, ascii_lowercase, ascii_uppercase
+from threading import Event, Lock
+from typing import Generator, List
+import array
+import ctypes
+import json
+import matplotlib.pyplot as plt
+import numpy
+import numpy as np
+import pytest
+import re
+import requests
+import statistics
+import sys
+import time
+import warnings
+
+
