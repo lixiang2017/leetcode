1550495eef534ca586203075e3efaf2f5443904a
lixiang2017 <laoxing201314@outlook.com>
2022-03-24 18:05:21 +0800

    explore 2022

--- cpp/0881.0_Boats_to_Save_People.cpp
+++ cpp/0881.0_Boats_to_Save_People.cpp
@@ -1,7 +1,7 @@
 /*
 sort + two pointers
 T: O(NlogN + N)
-S: O(1)
+S: O(logN), for quick sort
 
 Runtime: 129 ms, faster than 30.49% of C++ online submissions for Boats to Save People.
 Memory Usage: 41.9 MB, less than 91.53% of C++ online submissions for Boats to Save People.
