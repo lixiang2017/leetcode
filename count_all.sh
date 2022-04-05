3fdf80657b1ca3687e27095fedb9ddadf5cb436c
lixiang2017 <laoxing201314@outlook.com>
2022-04-05 23:32:11 +0800

    explore 2022

--- count_all.sh
+++ count_all.sh
@@ -3,5 +3,15 @@
 # include accpeted and attempted, the attempted will be a very small number.
 # not include `explore` topic in leetcode.com, so the result will less than real sum of accepted number in both websites.
 # not include `shell`/`lcp`/`sword2offer`/`CrackingTheCodingInterview` in leetcode-cn.com
+
+echo 'leetcode.com and leetcode-cn.com problems count: '
 ls problems leetcode-cn |  grep -E '^[0-9]' | awk '{print substr($1, 1, 4)}' | uniq |wc -l
 
+echo 'leetcode.com problems count: '
+ls problems |  grep -E '^[0-9]' | awk '{print substr($1, 1, 4)}' | uniq |wc -l
+
+echo 'leetcode-cn.com problems count: '
+ls leetcode-cn |  grep -E '^[0-9]' | awk '{print substr($1, 1, 4)}' | uniq |wc -l
+
+echo 'database count: '
+ls leetcode-cn/sql |  grep -E '^[0-9]' | awk '{print substr($1, 1, 4)}' | uniq |wc -l
