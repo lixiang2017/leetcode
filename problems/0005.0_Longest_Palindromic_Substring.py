c50b31ed90831d68e59eabc056eae37e0a28c1c5
lixiang <838255715@qq.com>
2022-06-21 00:08:23 +0800

    [update](2022-06-21 00:08:23): auto commit and push

---
+++ problems/0005.0_Longest_Palindromic_Substring.py
@@ -0,0 +1,29 @@
+'''
+中心扩散法
+T： O（N^2）
+S: O(1)
+
+Runtime: 3307 ms, faster than 21.00% of Python3 online submissions for Longest Palindromic Substring.
+Memory Usage: 13.9 MB, less than 59.59% of Python3 online submissions for Longest Palindromic Substring.
+'''
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        # longest left index and right index 
+        left = right = 0
+        for i in range(n):
+            # odd
+            step = 0
+            while 0 <= i - step < n and 0 <= i + step < n and s[i - step] == s[i + step]:
+                if 2 * step + 1 > right - left:
+                    left = i - step
+                    right = i + step + 1
+                step += 1
+            # even 
+            step = 0
+            while 0 <= i - step < n and 0 <= i + step + 1 < n and s[i - step] == s[i + step + 1]:
+                if 2 * step + 2 > right - left:
+                    left = i - step
+                    right = i + step +  2
+                step += 1
+        return s[left: right]
