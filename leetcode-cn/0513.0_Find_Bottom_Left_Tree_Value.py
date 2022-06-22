02da7037fca2ce5f46564a58890585f7f04e955c
lixiang <838255715@qq.com>
2022-06-23 00:52:29 +0800

    [update](2022-06-23 00:52:29): auto commit and push

--- leetcode-cn/0513.0_Find_Bottom_Left_Tree_Value.py
+++ leetcode-cn/0513.0_Find_Bottom_Left_Tree_Value.py
@@ -19,6 +19,33 @@ class Solution:
             q = [child for node in q for child in [node.left, node.right] if child]
         return ans 
 
+'''
+BFS2
+
+执行用时：52 ms, 在所有 Python3 提交中击败了52.25% 的用户
+内存消耗：17.6 MB, 在所有 Python3 提交中击败了53.70% 的用户
+通过测试用例：76 / 76
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        ans, q = -1, deque([root]) if root else deque()
+        while q:
+            node = q.popleft()
+            if node.right:
+                q.append(node.right)
+            if node.left:
+                q.append(node.left)
+            ans = node.val 
+        return ans 
+
+
+
 
 '''
 DFS
