227ebd3d73ec4551dcd49d13daa759366b721005
lixiang <838255715@qq.com>
2022-07-24 02:34:22 +0800

    [update](2022-07-24 02:34:22): auto commit and push

--- leetcode-cn/1184.0_Distance_Between_Bus_Stops.py
+++ leetcode-cn/1184.0_Distance_Between_Bus_Stops.py
@@ -10,4 +10,17 @@ class Solution:
             start, destination = destination, start
         one = sum(distance[start: destination])
         return min(one, s - one)
-        
+
+'''
+执行用时：40 ms, 在所有 Python3 提交中击败了40.33% 的用户
+内存消耗：15.6 MB, 在所有 Python3 提交中击败了41.67% 的用户
+通过测试用例：37 / 37
+'''
+class Solution:
+    def distanceBetweenBusStops(self, distance: List[int], start: int, destination: int) -> int:
+        s = sum(distance)
+        if start > destination:
+            start, destination = destination, start
+        s1 = sum(distance[start: destination])
+        return min(s1, s - s1)
+
