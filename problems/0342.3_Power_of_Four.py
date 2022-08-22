5c7be22e305effa356c6b1986326a75c1e080442
lixiang <838255715@qq.com>
2022-08-22 23:40:00 +0800

    [update](2022-08-22 23:39:59): auto commit and push

--- problems/0342.3_Power_of_Four.py
+++ problems/0342.3_Power_of_Four.py
@@ -5,3 +5,12 @@ Memory Usage: 13.9 MB, less than 10.18% of Python3 online submissions for Power 
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
         return n > 0 and n & (n - 1) == 0 and len(bin(n)) & 1
+
+
+'''
+Runtime: 49 ms, faster than 56.74% of Python3 online submissions for Power of Four.
+Memory Usage: 13.9 MB, less than 10.18% of Python3 online submissions for Power of Four.
+'''
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        return n > 0 and n & (n - 1) == 0 and n % 3 == 1
