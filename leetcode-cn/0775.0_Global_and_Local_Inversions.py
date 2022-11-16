ad0673cc950cf76babc5f931f575660973c8fbbe
lixiang2017 <laoxing201314@outlook.com>
2022-11-16 22:52:21 +0800

    [update](2022-11-16 22:52:21): auto commit and push

--- leetcode-cn/0775.0_Global_and_Local_Inversions.py
+++ leetcode-cn/0775.0_Global_and_Local_Inversions.py
@@ -1,18 +1,15 @@
 '''
-pre_max, nums[i - 1], nums[i]
-T: O(N)
-S: O(1)
-
-执行用时：152 ms, 在所有 Python3 提交中击败了25.00% 的用户
-内存消耗：25.2 MB, 在所有 Python3 提交中击败了62.07% 的用户
+执行用时：128 ms, 在所有 Python3 提交中击败了53.41% 的用户
+内存消耗：24.7 MB, 在所有 Python3 提交中击败了88.64% 的用户
 通过测试用例：226 / 226
 '''
 class Solution:
     def isIdealPermutation(self, nums: List[int]) -> bool:
         n = len(nums)
-        pre_max = nums[0]
-        for i in range(2, n):
+        pre_max = -1
+        for i in range(1, n):
             if pre_max > nums[i]:
-                return False
+                return False 
             pre_max = max(pre_max, nums[i - 1])
         return True
+        
