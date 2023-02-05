f999fae2d7b7c8a7852ada6630773e74a70d8643
lixiang2017 <laoxing201314@outlook.com>
2023-02-06 00:17:46 +0800

    [update](2023-02-06 00:17:46): auto commit and push

--- problems/0438.0_Find_All_Anagrams_in_a_String.py
+++ problems/0438.0_Find_All_Anagrams_in_a_String.py
@@ -27,3 +27,46 @@ class Solution:
 
         return ans 
         
+
+'''
+bit mask
+
+Runtime: 157 ms, faster than 62.09% of Python3 online submissions for Find All Anagrams in a String.
+Memory Usage: 15.2 MB, less than 27.00% of Python3 online submissions for Find All Anagrams in a String.
+'''
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        pn = len(p)
+        cp = Counter(p)
+        diff_mask = 0
+        ans = []
+        for i, ch in enumerate(s):
+            if i < pn:
+                cp[ch] -= 1
+                if cp[ch] == 0:
+                    diff_mask &= ~(1 << (ord(ch) - ord('a')))
+                else:
+                    diff_mask |= (1 << (ord(ch) - ord('a')))
+                if i == pn - 1 and diff_mask == 0:
+                    ans.append(0)
+                continue
+                
+            # remove i - pn
+            left = s[i - pn]
+            cp[left] += 1
+            if cp[left] == 0:
+                diff_mask &= ~(1 << (ord(left) - ord('a')))
+            else:
+                diff_mask |= (1 << (ord(left) - ord('a')))
+            # add i 
+            cp[ch]-= 1
+            if cp[ch] == 0:
+                diff_mask &= ~(1 << (ord(ch) - ord('a')))
+            else:
+                diff_mask |= (1 << (ord(ch) - ord('a')))
+            
+            if diff_mask == 0:
+                ans.append(i - pn + 1)
+                
+        return ans
+
