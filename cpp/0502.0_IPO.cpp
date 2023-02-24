924069e490bb5b92c00d4bdd5f2e6ba7434a17b7
lixiang <laoxing201314@outlook.com>
2023-02-24 22:13:04 +0800

    [update](2023-02-24 22:13:03): auto commit and push

--- cpp/0502.0_IPO.cpp
+++ cpp/0502.0_IPO.cpp
@@ -26,3 +26,31 @@ public:
         return w;
     }
 };
+
+
+/*
+执行用时：204 ms, 在所有 C++ 提交中击败了27.94% 的用户
+内存消耗：74.8 MB, 在所有 C++ 提交中击败了91.50% 的用户
+通过测试用例：35 / 35
+*/
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        int n = profits.size();
+        vector<int> index(n);
+        for (int i = 0; i < n; i++) index[i] = i;
+        sort(index.begin(), index.end(), [&](int i, int j){
+            return capital[i] < capital[j];
+        });
+        priority_queue<int> q;
+        int i = 0;
+        while (k-- > 0) {
+            while (i < n && capital[index[i]] <= w) q.emplace(profits[index[i++]]);
+            if (q.empty()) break;
+            w += q.top();
+            q.pop();
+        }
+        return w;
+    }
+};
+
