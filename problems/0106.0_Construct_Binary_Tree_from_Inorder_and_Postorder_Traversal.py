4d139915c4fbcf62443d064b5aebb8054cd5c6a3
lixiang2017 <laoxing201314@outlook.com>
2023-03-18 17:00:54 +0800

    [update](2023-03-18 17:00:54): auto commit and push

--- problems/0106.0_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.py
+++ problems/0106.0_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.py
@@ -25,3 +25,65 @@ class Solution:
         root.right = self.buildTree(inorder[idx + 1: ], postorder[idx: -1])
         
         return root
+
+
+'''
+DFS
+
+Runtime: 99 ms, faster than 64.34% of Python3 online submissions for Construct Binary Tree from Inorder and Postorder Traversal.
+Memory Usage: 19 MB, less than 70.03% of Python3 online submissions for Construct Binary Tree from Inorder and Postorder Traversal.
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        n = len(inorder)
+        
+        def construct(inleft: int, inright: int, postleft: int, postright: int) -> Optional[TreeNode]:
+            if inleft > inright:
+                return None
+            val = postorder[postright]
+            node = TreeNode(val)
+            idx = inorder.index(val)
+            left_len = idx - inleft
+            # right_len = inright - idx
+            node.left = construct(inleft, idx - 1, postleft, postleft + left_len - 1)
+            node.right = construct(idx + 1, inright, postleft + left_len, postright - 1)
+            return node
+        
+        return construct(0, n - 1, 0, n - 1)
+
+
+'''
+Runtime: 97 ms, faster than 64.55% of Python3 online submissions for Construct Binary Tree from Inorder and Postorder Traversal.
+Memory Usage: 19 MB, less than 70.03% of Python3 online submissions for Construct Binary Tree from Inorder and Postorder Traversal.
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        n = len(inorder)
+        
+        def construct(inleft: int, inright: int, postleft: int, postright: int) -> Optional[TreeNode]:
+            if inleft > inright:
+                return None
+            val = postorder[postright]
+            node = TreeNode(val)
+            idx = inorder.index(val, inleft, inright + 1)
+            left_len = idx - inleft
+            node.left = construct(inleft, idx - 1, postleft, postleft + left_len - 1)
+            node.right = construct(idx + 1, inright, postleft + left_len, postright - 1)
+            return node
+        
+        return construct(0, n - 1, 0, n - 1)
+
+
+        
