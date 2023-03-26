838cbff0ae39386bd7bcb4176875f8bfd8e864ae
lixiang2017 <laoxing201314@outlook.com>
2023-03-26 18:15:24 +0800

    [update](2023-03-26 18:15:24): auto commit and push

--- problems/2316.0_Count_Unreachable_Pairs_of_Nodes_in_an_Undirected_Graph.py
+++ problems/2316.0_Count_Unreachable_Pairs_of_Nodes_in_an_Undirected_Graph.py
@@ -32,3 +32,76 @@ class Solution:
             total -= set_cnt[r]
             ans += set_cnt[r] * total
         return ans 
+
+
+'''
+DFS
+
+Runtime: 2183 ms, faster than 65.28% of Python3 online submissions for Count Unreachable Pairs of Nodes in an Undirected Graph.
+Memory Usage: 114.3 MB, less than 7.29% of Python3 online submissions for Count Unreachable Pairs of Nodes in an Undirected Graph.
+'''
+class Solution:
+    def countPairs(self, n: int, edges: List[List[int]]) -> int:
+        g = defaultdict(list)
+        for a, b in edges:
+            g[a].append(b)
+            g[b].append(a)
+            
+        all_nodes = set(range(n))
+        seen = set()
+
+        def dfs(x, parent):
+            for child in g[x]:
+                if child not in seen and child != parent:
+                    seen.add(child)
+                    all_nodes.remove(child)
+                    dfs(child, x)
+        
+        total = n
+        ans = 0
+        while all_nodes:
+            x = all_nodes.pop()
+            seen = {x}
+            dfs(x, -1)
+            total -= len(seen)
+            ans += len(seen) * total
+        return ans 
+        
+
+'''
+BFS
+
+Runtime: 2184 ms, faster than 64.24% of Python3 online submissions for Count Unreachable Pairs of Nodes in an Undirected Graph.
+Memory Usage: 75.5 MB, less than 62.50% of Python3 online submissions for Count Unreachable Pairs of Nodes in an Undirected Graph.
+'''
+class Solution:
+    def countPairs(self, n: int, edges: List[List[int]]) -> int:
+        g = defaultdict(list)
+        for a, b in edges:
+            g[a].append(b)
+            g[b].append(a)
+            
+        all_nodes = set(range(n))
+        seen = set()
+
+        def bfs(x):
+            q = deque([x])
+            while q:
+                node = q.popleft()
+                for child in g[node]:
+                    if child not in seen:
+                        q.append(child)
+                        seen.add(child)
+                        all_nodes.remove(child)
+        
+        total = n
+        ans = 0
+        while all_nodes:
+            x = all_nodes.pop()
+            seen = {x}
+            bfs(x)
+            total -= len(seen)
+            ans += len(seen) * total
+        return ans 
+        
+
