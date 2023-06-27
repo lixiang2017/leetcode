e83540434e99df3cb90f66a50c60c51c5c3ebda8
lixiang2017 <laoxing201314@outlook.com>
2023-06-28 00:31:11 +0800

    [update](2023-06-28 00:31:11): auto commit and push

--- problems/0373.0_Find_K_Pairs_with_Smallest_Sums.py
+++ problems/0373.0_Find_K_Pairs_with_Smallest_Sums.py
@@ -11,3 +11,76 @@ class Solution:
         ks = nsmallest(k, [(nums1[i] + nums2[j], nums1[i], nums2[j]) \
             for i in range(min(k, len(nums1))) for j in range(min(k, len(nums2)))])
         return [[x, y]for _sum, x, y in ks]
+
+
+'''
+Runtime: 2795 ms, faster than 5.04% of Python3 online submissions for Find K Pairs with Smallest Sums.
+Memory Usage: 35.4 MB, less than 63.07% of Python3 online submissions for Find K Pairs with Smallest Sums.
+'''
+class Solution:
+    def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
+        left, right = nums1[0] + nums2[0], nums1[-1] + nums2[-1]
+        
+        def check(mid):
+            # count sum(x and y) <= mid
+            cnt = 0
+            for x in nums1:
+                idx = bisect_right(nums2, mid - x)
+                cnt += idx
+            return cnt >= k
+        
+        while left <= right:
+            mid = left + (right - left) // 2
+            if check(mid):
+                right = mid - 1
+            else:
+                left = mid + 1
+        
+        target_sum = right + 1
+        ans = []
+        for x in nums1:
+            for y in nums2:
+                if x + y <= target_sum:
+                    ans.append([x, y])
+                else:
+                    break
+        
+        ans.sort(key=lambda pair: sum(pair))
+        return ans[: k]
+
+'''
+Runtime: 2785 ms, faster than 5.04% of Python3 online submissions for Find K Pairs with Smallest Sums.
+Memory Usage: 35.2 MB, less than 69.98% of Python3 online submissions for Find K Pairs with Smallest Sums.
+'''
+class Solution:
+    def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
+        left, right = nums1[0] + nums2[0], nums1[-1] + nums2[-1]
+        
+        def check(mid):
+            # count sum(x and y) <= mid
+            cnt = 0
+            for x in nums1:
+                idx = bisect_right(nums2, mid - x)
+                cnt += idx
+            return cnt >= k
+        
+        while left <= right:
+            mid = left + (right - left) // 2
+            if check(mid):
+                right = mid - 1
+            else:
+                left = mid + 1
+        
+        target_sum = right + 1
+        ans = []
+        equal = []
+        for x in nums1:
+            for y in nums2:
+                if x + y < target_sum:
+                    ans.append([x, y])
+                elif x + y == target_sum:
+                    equal.append([x, y])
+                else:
+                    break
+        
+        return ans + equal[: k - len(ans)]
