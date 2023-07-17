c84cada23c1f33027f051ff40021bb7fc2ae471d
lixiang2017 <laoxing201314@outlook.com>
2023-07-17 23:55:11 +0800

    [update](2023-07-17 23:55:10): auto commit and push

--- leetcode-cn/0415.0_Add_Strings.py
+++ leetcode-cn/0415.0_Add_Strings.py
@@ -66,4 +66,45 @@ class Solution:
         
 
 
-        
+'''
+执行用时：68 ms, 在所有 Python3 提交中击败了7.58% 的用户
+内存消耗：16.4 MB, 在所有 Python3 提交中击败了13.44% 的用户
+通过测试用例：317 / 317
+'''       
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        chars = []
+        carry = 0
+        it1, it2 = iter(reversed(num1)), iter(reversed(num2))
+        while True:
+            x1 = next(it1, None)
+            x2 = next(it2, None)
+            if x1 is None and x2 is None and 0 == carry:
+                break
+            s = (int(x1) if x1 is not None else 0) + (int(x2) if x2 is not None else 0) + carry
+            carry, x = divmod(s, 10)
+            chars.append(str(x))
+
+        return ''.join(reversed(chars)) or "0"
+
+
+'''
+执行用时：48 ms, 在所有 Python3 提交中击败了75.11% 的用户
+内存消耗：16.6 MB, 在所有 Python3 提交中击败了5.26% 的用户
+通过测试用例：317 / 317
+'''
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        chars = []
+        carry = 0
+        it1, it2 = iter(reversed(num1)), iter(reversed(num2))
+        while True:
+            x1 = next(it1, None)
+            x2 = next(it2, None)
+            if x1 is None and x2 is None and 0 == carry:
+                break
+            s = (int(x1) if x1 is not None else 0) + (int(x2) if x2 is not None else 0) + carry
+            carry, x = divmod(s, 10)
+            chars.append(str(x))
+
+        return ''.join(reversed(chars))
