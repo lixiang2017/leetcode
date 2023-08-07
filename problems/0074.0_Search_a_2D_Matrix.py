6aec9449b6ce795913c97ba6b14d4a6dd15da529
lixiang2017 <laoxing201314@outlook.com>
2023-08-08 00:03:15 +0800

    [update](2023-08-08 00:03:15): auto commit and push

--- problems/0074.0_Search_a_2D_Matrix.py
+++ problems/0074.0_Search_a_2D_Matrix.py
@@ -20,3 +20,26 @@ class Solution:
             else:
                 j = mid - 1
         return False
+
+'''
+binary search
+T: O(log(MN))
+S: O(1)
+
+Runtime: 54 ms, faster than 76.18% of Python3 online submissions for Search a 2D Matrix.
+Memory Usage: 16.8 MB, less than 64.57% of Python3 online submissions for Search a 2D Matrix.
+'''
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m, n = len(matrix), len(matrix[0])
+        left, right = 0, m * n - 1
+        while left <= right:
+            mid = (left + right) // 2
+            i, j = divmod(mid, n)
+            if matrix[i][j] == target:
+                return True
+            elif matrix[i][j] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return False
