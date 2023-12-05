78a18b8a6a0a3aec1f9a9b8e73c8de6756434dd8
lixiang2017 <laoxing201314@outlook.com>
2023-12-05 18:41:25 +0800

    [update](2023-12-05 18:41:22): auto commit and push

---
+++ adventofcode/2023/day5/part1/lowest_location.py
@@ -0,0 +1,85 @@
+import os
+
+path = os.path.join(os.path.dirname(__file__), "input")
+
+seeds = []
+seed_to_soil = []
+soil_to_fertilizer = []
+fertilizer_to_water = []
+water_to_light = []
+light_to_temperature = []
+temperature_to_humidity = []
+humidity_to_location = []
+
+
+def get_map_data(f):
+    data = []
+    while True:
+        line = f.readline().strip()
+        if line and line[0].isdigit():
+            # three numbers: the destination range start, the source range start, and the range length.
+            destination, source, length = list(map(int, line.split()))
+            data.append([destination, source, length])
+        else:
+            break 
+    return data 
+
+with open(path) as f:
+    line = f.readline()
+    if line.startswith('seeds:'):
+        seeds = list(map(int, line.split(":")[1].split()))
+    
+    while line and not line.startswith("seed-to-soil map:"):
+        line = f.readline()
+    seed_to_soil = get_map_data(f)
+    
+    while line and not line.startswith("soil-to-fertilizer map:"):
+        line = f.readline()
+    soil_to_fertilizer = get_map_data(f)
+    
+    while line and not line.startswith("fertilizer-to-water map:"):
+        line = f.readline()
+    fertilizer_to_water = get_map_data(f)
+
+    while line and not line.startswith("water-to-light map:"):
+        line = f.readline()
+    water_to_light = get_map_data(f)
+    
+    while line and not line.startswith("light-to-temperature map:"):
+        line = f.readline()
+    light_to_temperature = get_map_data(f)
+    
+    while line and not line.startswith("temperature-to-humidity map:"):
+        line = f.readline()
+    temperature_to_humidity = get_map_data(f)
+    
+    while line and not line.startswith("humidity-to-location map:"):
+        line = f.readline()
+    humidity_to_location = get_map_data(f)
+
+# print(seed_to_soil)
+# print(soil_to_fertilizer)
+# print(fertilizer_to_water)
+# print(water_to_light)
+# print(light_to_temperature)
+# print(temperature_to_humidity)
+# print(humidity_to_location)
+
+def mapping(seeds, m):
+    new_seeds = []
+    for s in seeds:
+        found = False
+        for destination, source, length in m:
+            if source <= s < source + length:
+                delta = s - source 
+                new_seeds.append(destination + delta)
+                found = True 
+                break
+        if not found:
+            new_seeds.append(s)
+    return new_seeds
+
+for m in [seed_to_soil, soil_to_fertilizer, fertilizer_to_water, water_to_light, light_to_temperature, temperature_to_humidity, humidity_to_location]:
+    seeds = mapping(seeds, m)
+
+print('ans ', min(seeds)) # 35 # 551761867
