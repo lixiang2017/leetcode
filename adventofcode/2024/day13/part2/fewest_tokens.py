0b9e939ebf18338d7d7ff8cf9ce09bae83132e8b
lixiang2017 <laoxing201314@outlook.com>
2024-12-13 16:11:35 +0800

    [update](2024-12-13 16:11:35): auto commit and push

--- adventofcode/2024/day13/part2/fewest_tokens.py
+++ adventofcode/2024/day13/part2/fewest_tokens.py
@@ -15,16 +15,8 @@ y = (s * b1 - t * a1) / (a2 * b1 - a1 * b2)
 
 def get_x_y(s):
     x_part, y_part = s.strip().split(":")[1].split(",")
-    # print(x_part.strip())
-    # print(y_part.strip())
-    if "+" in x_part:
-        x_str = x_part.strip().split("+")[1]
-    else:
-        x_str = x_part.strip().split("=")[1]
-    if "+" in y_part:
-        y_str = y_part.strip().split("+")[1]
-    else:
-        y_str = y_part.strip().split("=")[1]
+    x_str = re.split(r"\+|=", x_part.strip())[1]
+    y_str = re.split(r"\+|=", y_part.strip())[1]
     return int(x_str), int(y_str)
 
 ans = 0
@@ -32,8 +24,6 @@ file_path = os.path.join(os.path.dirname(__file__), 'input')
 lines = Path(file_path).read_text(encoding="utf-8")
 groups = re.split("\n\n", lines)
 for group in groups:
-    print("++++++++++")
-    # print(group)
     A, B, P = group.split("\n")
     a1, b1 = get_x_y(A)
     a2, b2 = get_x_y(B)
@@ -46,7 +36,7 @@ for group in groups:
     if 0 <= x <= 100 and 0 <= y <= 100:
         x, y = int(x), int(y)
         if a1 * x + a2 * y == s and b1 * x + b2 * y == t:
-            print("xy ", x, y)
+            # print("xy ", x, y)
             ans += 3 * x + y
     
 print('ans:', int(ans)) # ans: 34787
