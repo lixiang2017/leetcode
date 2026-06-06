e03f157824ae3490f8622c4a1846a7f2a329c104
Songlet Li <laoxing201314@outlook.com>
2026-06-06 19:09:41 +0800

    Refactor leftRightDifference method for clarity

--- leetcode-cn/2574.0_Left_and_Right_Sum_Differences.py
+++ leetcode-cn/2574.0_Left_and_Right_Sum_Differences.py
@@ -8,3 +8,13 @@ class Solution:
             ans.append(abs(lefts - rights))
             lefts += x
         return ans
+
+
+class Solution:
+    def leftRightDifference(self, nums: List[int]) -> List[int]:
+        total = sum(nums)
+        left_sum = 0
+        for i, x in enumerate(nums):
+            nums[i] = abs(left_sum * 2 + x - total)
+            left_sum += x
+        return nums
